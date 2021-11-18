44fc731cd7a31d9a44200454123bbbff94136365
Dan Akmat <akmatdan@gmail.com>
2021-11-18 09:57:17 +0600

    url, request, urlSession

--- WeatherJun/ListTVC.swift
+++ WeatherJun/ListTVC.swift
@@ -15,6 +15,21 @@ class ListTVC: UITableViewController {
     }
 
     func fetchWeather() {
+        let urlString = "https://api.weather.yandex.ru/v2/forecast?lat=42.826639?lon=75.058796"
+        guard let url = URL(string: urlString) else { return }
+        
+        var request = URLRequest(url: url, timeoutInterval: Double.infinity)
+        request.addValue("ee701582-f2a2-475f-a141-d9087f7040cb", forHTTPHeaderField: "X-Yandex-API-Key")
+        request.httpMethod = "GET"
+        
+        let task = URLSession.shared.dataTask(with: request) {
+            (data, response, error) in
+            guard let data = data else {
+                print(String(describing: error))
+                return
+            }
+            print(String(data: data, encoding: .utf8))
+        }
         
     }
     // MARK: - Table view data source
